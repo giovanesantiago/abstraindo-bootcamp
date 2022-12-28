aa78c6447a19e641128c9ebd55db21baef1a73e0
giovanesantiago <giovanedevweb@gmail.com>
2022-12-27 23:03:26 -0300

    Criando get e set para as classes Curso e Mentoria

--- src/br/com/gio/bootcamp/dominio/Curso.java
+++ src/br/com/gio/bootcamp/dominio/Curso.java
@@ -8,4 +8,27 @@ public class Curso {
 
 
 
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
 }
--- src/br/com/gio/bootcamp/dominio/Mentoria.java
+++ src/br/com/gio/bootcamp/dominio/Mentoria.java
@@ -9,4 +9,28 @@ public class Mentoria {
     private LocalDate data;
 
 
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
 }
