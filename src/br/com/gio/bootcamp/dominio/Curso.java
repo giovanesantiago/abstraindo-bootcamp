5059a280feebc4b2ebe9f92391f077d29ab2e234
giovanesantiago <giovanedevweb@gmail.com>
2022-12-27 23:01:33 -0300

    Criando atributos para as classes Curso e Mentoria

---
+++ src/br/com/gio/bootcamp/dominio/Curso.java
@@ -0,0 +1,11 @@
+package br.com.gio.bootcamp.dominio;
+
+public class Curso {
+
+    private String titulo;
+    private String descricao;
+    private int cargaHoraria;
+
+
+
+}
---
+++ src/br/com/gio/bootcamp/dominio/Mentoria.java
@@ -0,0 +1,12 @@
+package br.com.gio.bootcamp.dominio;
+
+import java.time.LocalDate;
+
+public class Mentoria {
+
+    private String titulo;
+    private String descricao;
+    private LocalDate data;
+
+
+}
